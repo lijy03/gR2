6f3fc55e0becf1b0ef0d6aef8d22ae4d1ad06152
Heather Zhou <heatherjzhou@ucla.edu>
2020-04-02 17:48:11 -0700

    Changes to header files

--- src/2_update100TimesCpp.h
+++ src/2_update100TimesCpp.h
@@ -2,8 +2,11 @@
 #define update100TimesCpp_h
 
 #include <RcppArmadillo.h>
+#include <dqrng_distribution.h>
 using namespace arma;
 
-struct Struct1 update100TimesCpp(const arma::vec x,const arma::vec y,const int K,arma::vec membership,const std::string regressionMethod);
+struct Struct1 update100TimesCpp(const arma::vec x,const arma::vec y,
+                                 const int K,arma::vec membership,const std::string regressionMethod,const dqrng::rng64_t rng);
 
 #endif
+
--- src/3_EMUpdate100TimesCpp.h
+++ src/3_EMUpdate100TimesCpp.h
@@ -2,8 +2,11 @@
 #define EMUpdate100TimesCpp_h
 
 #include <RcppArmadillo.h>
+#include <dqrng_distribution.h>
 using namespace arma;
 
-arma::vec EMUpdate100TimesCpp(const arma::vec x,const arma::vec y,const int K,arma::vec membership);
+arma::vec EMUpdate100TimesCpp(const arma::vec x,const arma::vec y,
+                              const int K,arma::vec membership,const dqrng::rng64_t rng);
 
 #endif
+
